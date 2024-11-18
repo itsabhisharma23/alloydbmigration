d59f554fe49278712df620b91e64c84a15816e14
Abhi Sharma <iabhisharma@google.com>
2024-11-18 14:13:28 +0530

    removed figlet and banner

--- bin/banner.sh
+++ bin/banner.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Print banner
-sudo apt-get install -y figlet
-figlet PostgreSQL Migration to GCP
+echo "_________________________________________________________"
 echo 
 echo "$(tput setaf 2)Automated GCP migration tool for PostgreSQL to AlloyDB / CloudSQL$(tput setaf 7)"
 echo "Tool created by: Abhi Sharma & Dipinti Manandhar"
-echo "*********************************************************"
+echo "_________________________________________________________"
 echo -e "This tool can automate end-to-end PostgreSQL migration to CloudSQL or AlloyDB using Database Migration Service.\n"
+
+
