cebd1c582381ddcbcab5a208180c56bd618b8948
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-03-17 13:09:40 +0530

    Add files via upload

---
+++ dags/gen.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.utils.dates import days_ago
+
+def generate_dags():
+    default_args = {
+        'owner': 'airflow',
+        'start_date': days_ago(1),
+    }
+
+    items = ['item1', 'item2', 'item3']
+
+    dag = DAG(
+        dag_id='dynamic_example',
+        default_args=default_args,
+        schedule_interval=None,
+        catchup=False,
+    )
+
+    with dag:
+        for item in items:
+            task = BashOperator(
+                task_id=f'process_{item}',
+                bash_command=f'echo "Processing {item}"',
+            )
+
+    globals()[dag.dag_id] = dag
+
+generate_dags() #important, call the function to generate the dags.
