2b830ee9fa420b954f16bd51516b94951d012c13
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-03-17 14:42:47 +0530

    Update test_dynamic_dags.py

--- tests/test_dynamic_dags.py
+++ tests/test_dynamic_dags.py
@@ -1,41 +1,27 @@
 from dags import dynamic_dag_generator  # Import the DAG generator
-# Purpose: Pytest test for Data Lake and Redshift
-#          Airflow Demonstration project DAGs
-# Author: Gary A. Stafford
-# Modified: 2021-12-10
-
-import os
 import sys
+from pathlib import Path
 
 import pytest
 from airflow.models import DagBag
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../dags"))
-sys.path.append(os.path.join(os.path.dirname(__file__), "../dags/utilities"))
+# Import your dynamic DAG generator
+import dynamic_dag_generator
 
-# Airflow variables called from DAGs under test are stubbed out
-os.environ["AIRFLOW_VAR_DATA_LAKE_BUCKET"] = "test_bucket"
-os.environ["AIRFLOW_VAR_ATHENA_QUERY_RESULTS"] = "SELECT 1;"
-os.environ["AIRFLOW_VAR_SNS_TOPIC"] = "test_topic"
-os.environ["AIRFLOW_VAR_REDSHIFT_UNLOAD_IAM_ROLE"] = "test_role_1"
-os.environ["AIRFLOW_VAR_GLUE_CRAWLER_IAM_ROLE"] = "test_role_2"
+@pytest.fixture(scope="session")
+def dagbag():
+    dags_path = str((Path(__file__).parent.parent / "dags").resolve())
+    sys.path.insert(0, dags_path)
 
-
-@pytest.fixture(params=["../dags/"])
-def dag_bag(request):
+    # Generate dynamic DAGs before creating the DagBag
     dynamic_dag_generator.generate_dags()
-    yield DagBag(dag_folder=request.param, include_examples=False)
+
+    yield DagBag(dag_folder=dags_path, include_examples=False)
 
 def test_dagbag_not_empty(dagbag):
-    for dag in dagbag.dags.values():
-        print(dag.dag_id)
+    print("Testing DAGs: Ensuring dagbag is not empty")
     assert dagbag.size() > 0, "Dagbag should not be empty."
-    
+
 def test_dagbag_no_import_errors(dagbag):
+    print("Testing DAGs: Ensuring no import errors")
     assert dagbag.import_errors == {}, "No import errors should be found."
-
-    
-
-
-
-
