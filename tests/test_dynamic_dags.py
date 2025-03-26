a299a850c0c0569731682d720794453b6e9ddefd
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-03-27 00:14:50 +0530

    Update test_dynamic_dags.py

--- tests/test_dynamic_dags.py
+++ tests/test_dynamic_dags.py
@@ -1,79 +1,61 @@
 from dags import dynamic_dag_generator  # Import the DAG generator
-# Purpose: Pytest test for Data Lake and Redshift
-#          Airflow Demonstration project DAGs
-# Author: Gary A. Stafford
-# Modified: 2021-12-10
-
-import os
 import sys
-
+from pathlib import Path
 import pytest
 from airflow.models import DagBag
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../dags"))
-sys.path.append(os.path.join(os.path.dirname(__file__), "../dags/utilities"))
+@pytest.fixture(scope="session")
+def dagbag():
+    dags_path = str((Path(__file__).parent.parent / "dags").resolve())
+    sys.path.insert(0, dags_path)
 
-# Airflow variables called from DAGs under test are stubbed out
-os.environ["AIRFLOW_VAR_DATA_LAKE_BUCKET"] = "test_bucket"
-os.environ["AIRFLOW_VAR_ATHENA_QUERY_RESULTS"] = "SELECT 1;"
-os.environ["AIRFLOW_VAR_SNS_TOPIC"] = "test_topic"
-os.environ["AIRFLOW_VAR_REDSHIFT_UNLOAD_IAM_ROLE"] = "test_role_1"
-os.environ["AIRFLOW_VAR_GLUE_CRAWLER_IAM_ROLE"] = "test_role_2"
+    # Generate dynamic DAGs before creating the DagBag
+    dynamic_dag_generator.generate_dags()
 
-
-@pytest.fixture(params=["../dags/"])
-def dag_bag(request):
-    return DagBag(dag_folder=request.param, include_examples=False)
+    yield DagBag(dag_folder=dags_path, include_examples=False)
 
 
+def test_dagbag_not_empty(dagbag):
+    """Test if dagbag is non-empty"""
+    assert dagbag.size() > 0, "Dagbag should not be empty."
+    
 def test_no_import_errors(dag_bag):
+    """Test if there are any import errors"""
     assert not dag_bag.import_errors
 
 
 def test_requires_tags(dag_bag):
+    """Test if tags are available"""
     for dag_id, dag in dag_bag.dags.items():
         assert dag.tags
 
 
 def test_requires_specific_tag(dag_bag):
+    """Test if there are any required tags"""
     for dag_id, dag in dag_bag.dags.items():
-        try:
-            assert dag.tags.index("data lake demo") >= 0
-        except ValueError:
-            assert dag.tags.index("redshift demo") >= 0
-
-
-def test_desc_len_greater_than_fifteen(dag_bag):
-    for dag_id, dag in dag_bag.dags.items():
-        assert len(dag.description) > 15
-
-
-def test_owner_len_greater_than_five(dag_bag):
-    for dag_id, dag in dag_bag.dags.items():
-        assert len(dag.owner) > 5
+        assert dag.tags.index("environment") >= 0
 
 
 def test_owner_not_airflow(dag_bag):
+    """Test if there is a owner for the dag except 'airflow'"""
     for dag_id, dag in dag_bag.dags.items():
         assert str.lower(dag.owner) != "airflow"
-
-
-def test_no_emails_on_retry(dag_bag):
-    for dag_id, dag in dag_bag.dags.items():
-        assert not dag.default_args["email_on_retry"]
-
+        
 
 def test_no_emails_on_failure(dag_bag):
+    """Test if there are email_on_failure configured"""
     for dag_id, dag in dag_bag.dags.items():
         assert not dag.default_args["email_on_failure"]
 
 
 def test_three_or_less_retries(dag_bag):
+    """Test if there are 3 or less retries"""
     for dag_id, dag in dag_bag.dags.items():
         assert dag.default_args["retries"] <= 3
 
 
 def test_dag_id_contains_prefix(dag_bag):
+    """Test if there is a required prefix or not""
     for dag_id, dag in dag_bag.dags.items():
         assert str.lower(dag_id).find("__") != -1
 
