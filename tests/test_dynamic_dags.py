e9852c056d3d670ddcd2a336d8361f7a8c300bfd
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-03-27 00:26:23 +0530

    Update test_dynamic_dags.py

--- tests/test_dynamic_dags.py
+++ tests/test_dynamic_dags.py
@@ -19,44 +19,44 @@ def test_dagbag_not_empty(dagbag):
     """Test if dagbag is non-empty"""
     assert dagbag.size() > 0, "Dagbag should not be empty."
     
-def test_no_import_errors(dag_bag):
+def test_no_import_errors(dagbag):
     """Test if there are any import errors"""
     assert dagbag.import_errors == {}, "No import errors should be found."
 
 
-def test_requires_tags(dag_bag):
+def test_requires_tags(dagbag):
     """Test if tags are available"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert dag.tags
 
 
-def test_requires_specific_tag(dag_bag):
+def test_requires_specific_tag(dagbag):
     """Test if there are any required tags"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert dag.tags.index("environment") >= 0
 
 
-def test_owner_not_airflow(dag_bag):
+def test_owner_not_airflow(dagbag):
     """Test if there is a owner for the dag except 'airflow'"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert str.lower(dag.owner) != "airflow"
         
 
-def test_no_emails_on_failure(dag_bag):
+def test_no_emails_on_failure(dagbag):
     """Test if there are email_on_failure configured"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert not dag.default_args["email_on_failure"]
 
 
-def test_three_or_less_retries(dag_bag):
+def test_three_or_less_retries(dagbag):
     """Test if there are 3 or less retries"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert dag.default_args["retries"] <= 3
 
 
-def test_dag_id_contains_prefix(dag_bag):
+def test_dag_id_contains_prefix(dagbag):
     """Test if there is a required prefix or not"""
-    for dag_id, dag in dag_bag.dags.items():
+    for dag_id, dag in dagbag.dags.items():
         assert str.lower(dag_id).find("__") != -1
 
 
