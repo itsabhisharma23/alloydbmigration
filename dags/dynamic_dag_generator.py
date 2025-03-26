758871621735e4addf802478cb111c4391cb17d4
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-03-27 00:21:07 +0530

    Update dynamic_dag_generator.py

--- dags/dynamic_dag_generator.py
+++ dags/dynamic_dag_generator.py
@@ -15,6 +15,7 @@ def generate_dags():
         dag_id='dynamic_example',
         default_args=default_args,
         schedule_interval=None,
+        tags=["environment"]
         catchup=False,
     )
 
