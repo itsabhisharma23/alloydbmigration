585c8f673086f9160f29bf369055dd4a9cfd7546
Abhi Sharma <149653140+itsabhisharma23@users.noreply.github.com>
2025-04-02 03:17:41 +0530

    Create daga2.py

---
+++ src/python/cloud_composer/dags/daga2.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+with DAG(
+    dag_id='daga2',
+    default_args=default_args,
+    schedule_interval=None,
+    tags=["environment"],
+    catchup=False,
+) as dag:
+    task_1 = BashOperator(
+        task_id='process_item1',
+        bash_command='echo "Processing item1"',
+    )
+
+    task_2 = BashOperator(
+        task_id='process_item2',
+        bash_command='echo "Processing item2"',
+    )
+
+    task_3 = BashOperator(
+        task_id='process_item3',
+        bash_command='echo "Processing item3"',
+    )
+
+    # Define task dependencies (optional, but good practice)
+    task_1 >> task_2 >> task_3
