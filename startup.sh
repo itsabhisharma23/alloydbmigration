1a0ecd67e48b4998043fe3c4111cfc388594ce0a
Abhi Sharma <iabhisharma@google.com>
2025-04-02 15:38:23 +0530

    DMS job changes complete

--- startup.sh
+++ startup.sh
@@ -99,7 +99,7 @@ fi
 # Provide --cloudsql-instance if source DB is CLoudSQL(Postgre) else provide --alloydb-cluster property.
 
 if (( target_type_name == "AlloyDB" )); then
-    echo "\n${YELLOW}creating destination profile for AlloyDB.${NC}\n"
+    echo "\n${YELLOW}creating destination profile for AlloyDB...${NC}\n"
     gcloud database-migration connection-profiles create postgresql $DESTINATION_PROFILE_NAME \
     --region=$REGION \
     --display-name=$DESTINATION_PROFILE_NAME \
@@ -110,7 +110,7 @@ if (( target_type_name == "AlloyDB" )); then
     --prompt-for-password \
     --project=$PROJECT_ID
 else
-    echo "\n${YELLOW}creating destination profile for CloudSQL.${NC}\n"
+    echo "\n${YELLOW}creating destination profile for CloudSQL...${NC}\n"
     gcloud database-migration connection-profiles create postgresql $DESTINATION_PROFILE_NAME \
     --region=$REGION \
     --display-name=$DESTINATION_PROFILE_NAME \
@@ -129,4 +129,118 @@ else
   echo "${RED}Error: Failed to create connection profile \"${BOLD}$DESTINATION_PROFILE_NAME${NC}${RED}\".${NC}"
   exit 1
 fi
+
+
+# Create Migration Job
+
+echo "${YELLOW}creating database migration job...${NC}"
+
+# Create migration job 
+# For VPC Peering (test)
+# For Reverse-SSH Proxy provide the additional properties of --vm, --vm-ip, --vm-port and --vpc in the migration.config file
+
+# Print Notes:
+echo "${BOLD}\nNote: By Default the tool uses VPC peering.${NC}"
+echo "${BOLD}For Reverse-SSH Proxy provide the additional properties of --vm, --vm-ip, --vm-port and --vpc in the migration.config file\n${NC}"
+
+if [ -v VM_NAME ]; then
+    gcloud database-migration migration-jobs create $MIGRATION_JOB_NAME \
+    --region=$REGION \
+    --type=$MIGRATION_TYPE \
+    --source=$SOURCE_PROFILE_NAME \
+    --destination=$DESTINATION_PROFILE_NAME \
+    --project=$PROJECT_ID --vm=$VM_NAME --vm-ip=$VM_IP_ADDRESS --vm-port=$VM_PORT --vpc=$VPC
+else
+    gcloud database-migration migration-jobs create $MIGRATION_JOB_NAME \
+    --region=$REGION \
+    --type=$MIGRATION_TYPE \
+    --source=$SOURCE_PROFILE_NAME \
+    --destination=$DESTINATION_PROFILE_NAME \
+    --peer-vpc=$MIGRATION_NETWORK_FQDN \
+    --project=$PROJECT_ID # --vm=$VM_NAME --vm-ip=$VM_IP_ADDRESS --vm-port=$VM_PORT --vpc=$VPC
+fi
+
+
+
+# Check if the dms job creation was successful
+if [ $? -eq 0 ]; then
+  echo "${GREEN}DMS job $MIGRATION_JOB_NAME created successfully.${NC}"
+else
+  echo "${RED}Error: Failed to create DMS job $MIGRATION_JOB_NAME.${NC}"
+  exit 1
+fi
+
+echo "${YELLOW}Waiting for DMS job to be in ready state...${NC}"
+
+# wait for DMS creation
+while true; do
+    STATUS=$(gcloud database-migration migration-jobs describe "$MIGRATION_JOB_NAME" --region="$REGION" --project="$PROJECT_ID" --format='value(state)')
+
+    if [[ "$STATUS" == "NOT_STARTED" ]]; then
+        echo "Migration job '$MIGRATION_JOB_NAME' has changed state to: $STATUS"
+        break  # Exit the loop when the state is no longer NOT_STARTED
+    else
+        echo "Migration job '$MIGRATION_JOB_NAME' is still not ready. Waiting..."
+        sleep 10  # Wait for 10 seconds before checking again
+    fi
+done
+
+if (( target_type_name == "AlloyDB" )); then
+  # Demote the destination before starting the job
+  gcloud database-migration migration-jobs demote-destination $MIGRATION_JOB_NAME --region=$REGION --project=$PROJECT_ID
+
+  echo "\n${YELLOW}waiting for destination to be in demoted state...${NC}"
+  # wait for destination demotion
+
+  while true; do
+      STATUS=$(gcloud alloydb clusters describe $DESTINATION_ALLOYDB --region=$REGION --project=$PROJECT_ID --format="value(state)")
+
+      if [[ "$STATUS" == "BOOTSTRAPPING" ]]; then
+          echo "${GREEN}${BOLD}Destination is demoted.${NC}"
+          break  # Exit the loop when the state is no longer NOT_STARTED
+      else
+          echo "Waiting for destination to be in demoted state..."
+          sleep 10  # Wait for 10 seconds before checking again
+      fi
+  done
+else
+  # Demote the destination before starting the job
+  gcloud database-migration migration-jobs demote-destination $MIGRATION_JOB_NAME --region=$REGION --project=$PROJECT_ID
+  while true; do
+      MASTER_TYPE=$(gcloud sql instances describe $DESTINATION_CloudSQL_INSTANCE_NAME --project=$PROJECT_ID --format='value(instanceType)')
+      if [[ "$MASTER_TYPE" == "READ_REPLICA_INSTANCE" ]]; then
+          echo "${GREEN}${BOLD}Destination is demoted.${NC}"
+          break  # Exit the loop when the state is no longer NOT_STARTED
+      else
+          echo "Waiting for destination to be in demoted state..."
+          sleep 10  # Wait for 10 seconds before checking again
+      fi
+  done
+fi
+
+echo "\n\n${BOLD}Migration Job details${NC}"
+
+gcloud database-migration migration-jobs describe $MIGRATION_JOB_NAME --region=$REGION --project=$PROJECT_ID 
+
+echo "\n${YELLOW}Starting the DMS job...${NC}"
+
+#Start DMS Job
+gcloud database-migration migration-jobs start $MIGRATION_JOB_NAME --region=$REGION --project=$PROJECT_ID
+
+echo ""
+while true; do
+    STATUS=$(gcloud database-migration migration-jobs describe "$MIGRATION_JOB_NAME" --region="$REGION" --project="$PROJECT_ID" --format='value(state)')
+
+    if [[ "$STATUS" == "FAILED" ]]; then
+        echo "${RED}Migration job '$MIGRATION_JOB_NAME' has FAILED. Please check the job on the console.${NC}"
+        break  # Exit the loop when the state is no longer NOT_STARTED
+    elif [[ "$STATUS" == "RUNNING" ]]; then
+        echo "${BOLD}Migration job '$MIGRATION_JOB_NAME' is running. Please check the job on the console.${NC}"
+        break
+    else
+        echo "Migration job '$MIGRATION_JOB_NAME' has started. Waiting to be in running state..."
+        sleep 10  # Wait for 10 seconds before checking again
+    fi
+done
+
 exit 0 # Exit with a success code
